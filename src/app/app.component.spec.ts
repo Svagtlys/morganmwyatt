0f31dbdf33388c05ff1612cc3e134a61d87dcfbe
Svagtlys <45446348+Svagtlys@users.noreply.github.com>
2020-01-29 10:01:56 -0600

    Added TODO list

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testAngularPage'`, () => {
+  it(`should have as title 'The Beaten Path'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testAngularPage');
+    expect(app.title).toEqual('The Beaten Path');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testAngularPage app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('The Beaten Path app is running!');
   });
 });
